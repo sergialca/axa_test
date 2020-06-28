bf27d129787cd63ceccb6ccdfb1b16a0f2263132
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-06-28 09:37:03 +0200

    change routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/home" component={Home} />
-        <Redirect from="/" to="/home" />
+        <Route path="/project-cards" component={Home} />
+        <Redirect from="/" to="/project-cards" />
       </Switch>
     </div>
   );
