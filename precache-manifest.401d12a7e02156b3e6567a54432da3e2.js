abc276f2f83c35501ed54e6fbc5fc4b2a1b46f79
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-06-28 09:19:47 +0200

    Updates

---
+++ precache-manifest.401d12a7e02156b3e6567a54432da3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc681aae3a783bd2f07dc50bfe566314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03125417be4244f27073",
+    "url": "/static/css/main.f2012d83.chunk.css"
+  },
+  {
+    "revision": "ead89c3ec8a31cf30bcd",
+    "url": "/static/js/2.5e0e8210.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5e0e8210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03125417be4244f27073",
+    "url": "/static/js/main.01d7db5d.chunk.js"
+  },
+  {
+    "revision": "def697aff3f4a95fd74a",
+    "url": "/static/js/runtime-main.d4f6d621.js"
+  }
+]);
